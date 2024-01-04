aa6270f10e588f98c2a9f5158e8ed20cc49be23f
Samuel <mely@MacBook-Pro-de-Samuel.local>
2024-01-04 17:23:09 -0600

    correccion en tratamiento de respuesta de axios

--- src/pages/bussines/Page.jsx
+++ src/pages/bussines/Page.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import HTMLRenderer from 'react-html-renderer'
 const Page = (props)=>{
     const {data} = props
     console.log(data)
 return(<>
+<Helmet>
+<meta
+      property="og:image"
+      content={data.data.imagen}
+    />
+                  <title>{data.data.titulo}</title>
+                <meta name="description" content={data.data.descripcion}/>
+                <meta name="keywords" content={data.data.keyword} />
+                <link rel="canonical" href={data.data.nombre} />
+</Helmet>
         <div>
             <div className="cabecera">
                 <img src={data.data.imagen}/>
